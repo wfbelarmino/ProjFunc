5088cefbdfe437a2f2ea6ce6ee7dfc961d39c03a
wfbelarmino <wfbelarmino@gmail.com>
2022-01-12 20:07:05 -0300

    Criação do Crud incluir e excluir grupo projeto

--- ProjFunc/Controllers/CadastroController.cs
+++ ProjFunc/Controllers/CadastroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjFunc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,79 @@ namespace ProjFunc.Controllers
 {
     public class CadastroController : Controller
     {
+        private static List<GrupoProdutoModel> _listaGrupoProduto = new List<GrupoProdutoModel>()
+        {
+            new GrupoProdutoModel() { Id=1, Nome="Livros", Ativo=true },
+            new GrupoProdutoModel() { Id=2, Nome="Mouses", Ativo=true },
+            new GrupoProdutoModel() { Id=3, Nome="Monitores", Ativo=false }
+        };
+
         [Authorize]
         public ActionResult GrupoProduto()
         {
-            return View();
+            return View(_listaGrupoProduto);
+        }
+        [HttpPost]
+        [Authorize]
+        public ActionResult RecuperarGrupoProduto(int id)
+        {
+            return Json(_listaGrupoProduto.Find(x => x.Id == id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult ExcluirGrupoProduto(int id)
+        {
+            var ret = false;
+
+            var registroBD = _listaGrupoProduto.Find(x => x.Id == id);
+            if (registroBD != null)
+            {
+                _listaGrupoProduto.Remove(registroBD);
+                ret = true;
+            }
+
+            return Json(ret);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
+        {
+            var resultado = "OK";
+            var mensagens = new List<string>();
+            var idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "AVISO";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    var registroBD = _listaGrupoProduto.Find(x => x.Id == model.Id);
+
+                    if (registroBD == null)
+                    {
+                        registroBD = model;
+                        registroBD.Id = _listaGrupoProduto.Max(x => x.Id) + 1;
+                        _listaGrupoProduto.Add(registroBD);
+                    }
+                    else
+                    {
+                        registroBD.Nome = model.Nome;
+                        registroBD.Ativo = model.Ativo;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    resultado = "ERRO";
+                }
+            }
+
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
         }
 
         [Authorize]
---
+++ ProjFunc/Models/GrupoProdutoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjFunc.Models
+{
+    public class GrupoProdutoModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Preencha o nome.")]
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
